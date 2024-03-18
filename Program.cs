180c89da3cd4ab42d6b3ef65a5c3872d507dc2cf
IHaveAStrongPassword <achen115@ucr.edu>
2024-03-17 19:22:36 -0700

    add bindings

--- Program.cs
+++ Program.cs
@@ -1,24 +1,61 @@
 ﻿using fx;
+using Microsoft.VisualBasic;
 using System;
+using System.CodeDom;
+using System.Collections;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Management;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 using Terminal.Gui;
+using YamlDotNet;
+using YamlDotNet.Serialization;
 
-string cwd = null;
-string save = "fx.txt";
+
+var userprofileHide = "%USERPROFILE";
+var userprofile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+var cwd = Environment.CurrentDirectory;
+var restricted = new HashSet<string>();
+var lastIndex = new Dictionary<string, int>();
+
+string save = "fx.json";
 void Save () {
-	var state = $"{cwd}";
-	File.WriteAllText(save, state);
+	File.WriteAllText(save, new Serializer().Serialize(new State() {
+		cwd = cwd,
+		restricted= restricted.Select(r => r.Replace(cwd, "%CWD%")).ToHashSet(),
+		lastIndex = lastIndex
+	}).Replace(userprofile, userprofileHide));
 }
+#if false
+File.Delete(save);
+#endif
 void Load () {
 	if(File.Exists(save)) {
-		var state = File.ReadAllText(save);
-		cwd = state;
+		try {
+			(cwd, restricted, lastIndex) = new Deserializer().Deserialize<State>(File.ReadAllText(save).Replace(userprofileHide, userprofile));
+
+			File.Delete(save);
+
+			restricted = new(restricted.Select(s => s.Replace("%CWD%", cwd)));
+			lastIndex = new(lastIndex);
+			//Debug.Assert(lastIndex != null);
+		} catch(Exception e) {
+		} finally {
+
+		}
 	}
 }
 AppDomain.CurrentDomain.ProcessExit += (a, e) => {
 	Save();
 };
+
+
+Command[] commands = [.. XElement.Load("Bindings.xml").Elements().Select(Command.Parse)];
 
 Application.Init();
 try {
@@ -29,29 +66,30 @@ try {
 }
 Window Create () {
 
+
 	var favData = new List<PathItem>();
 	
 	var cwdData = new List<PathItem>();
 	var cwdPrev = new Stack<string>();
 	var cwdNext = new Stack<string>();
+	var cwdRecall = (string)null;
 	var procData = new List<ProcItem>();
 
-	var lastIndex = new Dictionary<string, int>();
-
 	var window = new Explorer();
-
+	#region left
 	var freqView = new View() {
 		X = 0,
 		Y = 0,
-		Width = Dim.Percent(25),
-		Height = window.Height
+		Width = Dim.Percent(25)-1,
+		Height = Dim.Percent(50)
 	};
+	
 	var freqName = new Label() {
 		X = 0,
 		Y = 0,
 		Width = Dim.Width(freqView),
 		Height = Dim.Sized(1),
-		Text = "[Frequents]",
+		Text = "[Recents]",
 	};
 	var freqList = new ListView() {
 		X = 0,
@@ -68,21 +106,31 @@ Window Create () {
 		}
 	};
 
-
+	var clipView = new View() { };
+	#endregion
+	var lineLeft = new Window() {
+		X = Pos.Right(freqView)-1,
+		Y = 0,
+		Width = 1,
+		Height = Dim.Fill(1),
+		
+	};
+	#region center
 	var pathView = new View() {
 		X = Pos.Percent(25),
 		Y = 0,
 		Width = Dim.Percent(50),
 		Height = window.Height,
 	};
-	var pathName = new Label() {
+	var pathName = new TextField() {
 		X = 0,
 		Y = 0,
 		Width = Dim.Width(pathView),
 		Height = Dim.Sized(1),
-		Text = cwd
+		Text = cwd,
+		Enabled = false
 	};
-	var goPrev = new Button("<-") { X = Pos.Right(pathView) - 18, };
+	var goPrev = new Button("<-") { X = Pos.Right(pathName) -18, };
 	var goNext = new Button("->") { X = Pos.Right(goPrev), };
 	var goLeft = new Button("..") { X = Pos.Right(goNext), };
 	var pathList = new ListView() {
@@ -99,7 +147,14 @@ Window Create () {
 			Disabled = new(Color.Red, Color.Black)
 		}
 	};
-
+	#endregion
+	var lineRight = new Window() {
+		X = Pos.Right(pathView) ,
+		Y = 0,
+		Width = 1,
+		Height = Dim.Fill()
+	};
+	#region right
 	var procView = new View() {
 		X = Pos.Right(pathView),
 		Y = 0,
@@ -127,8 +182,8 @@ Window Create () {
 			Disabled = new(Color.Red, Color.Black)
 		}
 	};
-
-
+	#endregion
+	#region bottom
 	var termField = new TextField() {
 		X = 0,
 		Y = Pos.Bottom(pathView) - 1,
@@ -143,13 +198,13 @@ Window Create () {
 			HotNormal = new(Color.Red, Color.Black),
 		}
 	};
-
+	#endregion
 	Init();
 	var tree = new Dictionary<View, List<View>>() {
 		[freqView] = [freqName, freqList],
 		[pathView] = [pathName, goPrev, goNext, goLeft, pathList],
 		[procView] = [procName, procList],
-		[window] = [freqView, pathView, procView, termField],
+		[window] = [freqView, lineLeft, pathView, lineRight, procView, termField],
 	};
 	foreach(var (parent, children) in tree) {
 		children.ForEach(parent.Add);
@@ -158,7 +213,7 @@ Window Create () {
 	return window;
 	void Init () {
 		termField.SetFocus();
-		SetCwd(cwd ??= Environment.CurrentDirectory);
+		SetCwd(cwd);
 		UpdateButtons();
 		foreach(var b in new[] { goPrev, goNext, goLeft }) {
 			b.Enabled = true;
@@ -226,22 +281,37 @@ Window Create () {
 							GoLeft();
 							
 						},
+						',' => () => {
+							//Create new tab
+							if(cwdRecall != null) SetCwd(cwdRecall);
+						}
+						,
+						'.' => () => {
+							//cwdRecall = cwd;
+
+							ShowContext(cwd);
+						},
 						'"' => () => {
 							if(!GetSelectedItem(out var p) || p.dir) return;
-							var d = new Dialog("Edit", []);
+							var initial = File.ReadAllText(p.path);
+							var d = new Dialog("View", []);
 							d.Add(new TextView() {
 								X = 0,
 								Y = 0,
 								Width = Dim.Fill(),
 								Height = Dim.Fill(),
-								Text = File.ReadAllText(p.path)
+								Text = initial,
+								ReadOnly = true,
 							});
+
 
 							Application.Run(d);
 						},
 						'?' => () => {
 							if(!GetSelectedItem(out var p)) return;
-							var d = new Dialog("Properties", []) { Text = $"{p.name}" };
+							var d = new Dialog("Properties", []) {
+								Text = $"{p.name}"
+							};
 							d.KeyPress += e => {
 								e.Handled = true;
 								d.Running = false;
@@ -249,12 +319,24 @@ Window Create () {
 							Application.Run(d);
 						},
 						'/' => () => {
-							new ContextMenu(pathList, new([
-								new MenuItem("Properties", "View properties", () => { })
-								
-								])).Show();
+							if(!GetSelectedItem(out var p)) return;
+							ShowContext(p.path);
+						},
+						'~' => () => {
+							//set do-not-read
+
+							//C# solution viewer
+							//Git viewer
+							if(!GetSelectedItem(out var p)) return;
+							
+							if(!restricted.Remove(p.path)) {
+								restricted.Add(p.path);
+							}
+							SetCwd(cwd);
+							pathList.SetNeedsDisplay();
 						},
 						'!' => () => {
+							//need option to collapse single-directory chains / single-non-empty-directory chains
 							if(!GetSelectedItem(out var p)) {
 								return;
 							}
@@ -262,7 +344,14 @@ Window Create () {
 								favData.Add(p);
 							}
 							freqList.SetNeedsDisplay();
-						},
+						}
+						,
+						'@' =>  () => {
+							//Copy file path
+
+
+						}
+						,
 						>='a' and <='z' => () => {
 							var c = $"{(char)e.KeyEvent.KeyValue}";
 							var index = pathList.SelectedItem;
@@ -394,6 +483,33 @@ Window Create () {
 					_ => delegate { e.Handled = false; }
 				}))();
 			};
+
+
+			window.KeyPress += e => {
+				e.Handled = true;
+				((Action)(e.KeyEvent.KeyValue switch {
+					':' => () => {
+						if(termField.HasFocus)
+							return;
+						termField.SetFocus();
+					},
+					_ => delegate { e.Handled = false; }
+				}))();
+			};
+		}
+		void ShowContext(string path) {
+			IEnumerable<MenuItem> GetCommon () {
+				yield return new MenuItem("Properties", "", () => { });
+				if(Directory.Exists(path))
+					yield return new MenuItem("Remember", "", () => cwdRecall = path);
+				yield return new("Copy Path", "", () => { Clipboard.TrySetClipboardData(path); });
+				foreach(var c in commands) {
+					if(c.Accept(path))
+						yield return new MenuItem(c.name, "", () => Run(c.GetCmd(path)));
+				}
+			}
+
+			new ContextMenu(5, 5, new([..GetCommon()])) { }.Show();
 		}
 		bool GetSelectedItem (out PathItem p) {
 			p = null;
@@ -406,19 +522,35 @@ Window Create () {
 
 		void SetCwd (string s) {
 			try {
+
 				var paths = new List<PathItem>([
 					..Directory.GetDirectories(s).Select(
-							f => new PathItem(Path.GetFileName(f), f, true)),
+							f => new PathItem(Path.GetFileName(f), f, true, restricted.Contains(f))),
 						..Directory.GetFiles(s).Select(
-							f => new PathItem(Path.GetFileName(f), f, false))]);
+							f => new PathItem(Path.GetFileName(f), f, false, restricted.Contains(f) ))]);
+				/*
+				if(s == cwd) {
+					goto UpdateListing;
+				}
+				*/
 
 				lastIndex[cwd] = pathList.SelectedItem;
-				cwd = s;
-				pathName.Text = $"[{s}]";
+				cwd = Path.GetFullPath(s);
+
+				var anonymize = true;
+				var showCwd = cwd;
+				if(anonymize) {
+					showCwd = showCwd.Replace(userprofile, "%USERPROFILE%");
+				}
+				//Anonymize
+				pathName.Text = showCwd;
+				Console.Title = showCwd;
+
+				pathList.SelectedItem = Math.Min(cwdData.Count, lastIndex.GetValueOrDefault(cwd, 0));
+
+				UpdateListing:
 				cwdData.Clear();
 				cwdData.AddRange(paths);
-
-				pathList.SelectedItem = lastIndex.TryGetValue(s, out int index) ? index : 0;
 
 
 			} catch(UnauthorizedAccessException e) {
@@ -484,6 +616,7 @@ Window Create () {
 				return;
 			}
 			var i = cwdData[pathList.SelectedItem];
+			if(i.restricted) return;
 			if(i.dir) {
 				GoPath(i.path);
 			} else {
@@ -510,10 +643,96 @@ Window Create () {
 		}
 	}
 }
+public static class Parse {
+	public static void Init<T>(this XElement source) {
+		T dest = (T)Activator.CreateInstance(typeof(T));
+		typeof(T).GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+			.Select(property => (property, set: property.SetMethod))
+			.Where(pair => pair.set != null)
+			.ToList().ForEach(p => {
+				p.set.Invoke(dest, [source.Attributes(p.property.Name)]);
+			});
+	}
+	public static string A (this XElement e, string key) => e.Attribute(key).Value;
+	public static string? TA (this XElement e, string key) => e.Attribute(key)?.Value;
+}
+public record Command(string name, string cmd, ITarget[] targets) {
+	public static Command Parse(XElement e) {
+		var name = e.A("name");
+		var cmd = e.A("cmd");
+		return new Command(name, cmd, [..e.Element("Target").Elements().Select(e => (ITarget)(e.Name.LocalName switch {
+			"Dir" => TargetDir.Parse(e),
+			"File" => TargetFile.Parse(e),
+			_ => throw new Exception()
+		}))]);
+	}
+	public bool Accept(string path) =>
+		targets.Any(t => t.Accept(path));
+	public string GetCmd (string path) => string.Format(cmd, path);
+}
+public interface ITarget {
+	public bool Accept(string path);
+}
+public record TargetFile([StringSyntax("Regex")] string pattern = ".+") : ITarget {
+	public static TargetFile Parse(XElement e) {
+		return new(e.TA("pattern") ?? $"[^\\.]*\\.{e.TA("ext")}");
+	}
+	public bool Accept (string path) {
+		return !Conditions().Contains(false);
+		IEnumerable<bool> Conditions () {
+			yield return File.Exists(path);
+			var f = Path.GetFileName(path);
+			var b = Regex.IsMatch(f, pattern);
+			yield return b;
+		}
+	}
+}
+public record TargetDir([StringSyntax("Regex")]string pattern = ".+") : ITarget {
+	public TargetFile[] subFile = [];
+	public TargetDir[] subDir = [];
+	public static TargetDir Parse(XElement e) {
+		return new(e.TA(nameof(pattern)) ?? ".+") {
+			subDir = [.. e.Elements("Dir").Select(TargetDir.Parse)],
+			subFile = [.. e.Elements("File").Select(TargetFile.Parse)]
+		};
+	}
+	public bool Accept (string path) {
+		return !Conditions().Contains(false);
+		IEnumerable<bool> Conditions () {
+			yield return Directory.Exists(path);
+			yield return Regex.IsMatch(Path.GetFileName(path), pattern);
 
+			var d = Directory.GetDirectories(path);
+			yield return subDir.All(s => d.Any(s.Accept));
+			
+			var f = Directory.GetFiles(path);
+			yield return subFile.All(s => f.Any(s.Accept));
+		}
+	}
+}
+public record TargetCombo (ITarget[] targets) {
+	public bool Accept (string[] paths, out string[] args) {
+		var remaining = new HashSet<string>(paths);
+		args = [..BindTargets()];
+		return args.Length == targets.Length;
+		IEnumerable<string> BindTargets() {
+			foreach(var t in targets) {
+				if(remaining.FirstOrDefault(t.Accept) is { } p) {
+					remaining.Remove(p);
+					yield return p;
+				} else {
+					yield break;
+				}
+			}
+		}
+	}
+}
 public record ProcItem(Process p) {
 	public override string ToString () => $"{p.ProcessName,-24}{p.Id, -8}";
 }
-public record PathItem (string name, string path, bool dir) {
-	public override string ToString () => $"{name,-32}{(dir ? "D" : "F")}";
+public record PathItem (string name, string path, bool dir, bool restricted) {
+	public override string ToString () => $"{$"{(restricted ? "~" : "")}{name}",-32}{(dir ? "D" : "F")}";
 }
+
+public record State (string cwd, HashSet<string> restricted, Dictionary<string, int> lastIndex) {
+	public State () : this(null, null, null) { } }
